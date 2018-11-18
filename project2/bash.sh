98d1f9663127f20c1049c3c4a141daf25bc35add
RainVisitor <abc873693@gmail.com>
2018-11-18 22:11:46 +0800

    Add project 2 bash and config

---
+++ project2/bash.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash 
+cd ~/Downloads/sniper-6.0/benchmarks
+declare -a l3Array=("4MB" "8MB" "16MB")
+declare -a coreArray=("1" "2")
+declare -a outFiles=("sim.info" "sim.out" "sim.cfg" "sim.stats.sqlite3" "cpi-stack.png" "power.png" "topo.svg" "power.xml" "power.txt" "power.py")
+#Problem a b
+cp ../hw2/nehalem.cfg ../config/nehalem.cfg
+for L3 in "${l3Array[@]}"
+do
+	for core in "${coreArray[@]}"
+	do
+		cp ../hw2/$L3.cfg ../config/gainestown.cfg
+		./run-sniper -p splash2-fft -i test -n $core -c gainestown
+		echo "--------------Core$core $L3 CPI Stacks--------------"
+		../tools/cpistack.py
+		echo "-------------Core$core $L3 Power Stacks-------------"
+		../tools/mcpat.py
+		../tools/gen_topology.py
+		dirName="Core$core-$L3"
+		mkdir -p $dirName
+		for fileName in "${outFiles[@]}"
+		do
+			mv $fileName "./$dirName/$fileName"
+		done
+	done
+done
+#Problem c
+cp "../hw2/gainestown(L4).cfg" ../config/gainestown.cfg
+cp "../hw2/nehalem(L4).cfg" ../config/nehalem.cfg
+./run-sniper -p splash2-fft -i test -n 1 -c gainestown
+echo "--------------Core1 L4=64MB CPI Stacks--------------"
+../tools/cpistack.py
+echo "-------------Core1 L4=64MB Power Stacks-------------"
+../tools/mcpat.py
+../tools/gen_topology.py
+dirName="Core1-L4"
+mkdir -p $dirName
+for fileName in "${outFiles[@]}"
+do
+	mv $fileName "./$dirName/$fileName"
+done
